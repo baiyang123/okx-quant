e6811419e0e38d184484041ed265023a2683121b
baiyang123 <1006925476@qq.com>
2024-12-18 20:52:16 +0800

    定时任务,模拟盘，持续优化，功能暂时齐全

--- test/ApiTest.py
+++ test/ApiTest.py
@@ -9,3 +9,20 @@ def test_market_tickers():
     headers = {}
     response = requests.request("get", url, headers=headers, json=param)
     print(response.text)
+
+# 如果需要代理 https://docs.ne.world/win/clash/  bing.com 搜设置cmd代理 request库http socks代理  地址127.0.0.1端口7890
+proxies = {
+    "http": "http://127.0.0.1:7890",
+    "https": "http://127.0.0.1:7890",
+}
+
+
+# 验证网络联通性
+def test_market_tickers():
+     url = LOCALHOST + "/api/v5/public/instruments?instId=BTC-USDT-SWAP&instType=SWAP"
+
+     response = requests.request('get', url, proxies=proxies)
+     print(response.text)
+
+if __name__ == '__main__':
+    test_market_tickers()
